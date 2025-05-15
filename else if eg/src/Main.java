b1c66b80d609b9098cb1910ba557d49ae5e5bd90
VINISHAKRISHNAMOORTHY <vinishakrishnamoorthy@gmail.com>
2025-05-15 16:31:19 +0530

    Add files via upload

---
+++ else if eg/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int studentpercentage = 78;
+    if(studentpercentage>=90){
+        System.out.println("Grade A");
+    }else if(studentpercentage>=60){
+        System.out.println("Grade B");
+    }else{
+        System.out.println("Grade C");
+    }
+    }
+}
---
+++ else if example/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[]args) {
+        int mark = 75;
+        if (mark >= 90) {
+            System.out.println("Grade A");
+        }else if (mark>=60) {
+            System.out.println("Grade B");
+        }else{
+            System.out.println("Grade C");
+        }
+    }
+}
