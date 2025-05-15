42c9bbdc8b435db9c0942446a3fae9cae4f46c2d
VINISHAKRISHNAMOORTHY <vinishakrishnamoorthy@gmail.com>
2025-05-15 17:31:04 +0530

    Add files via upload

---
+++ swtich case/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("sunday");
+                break;
+            case 2:
+                System.out.println("monday");
+                break;
+            default:
+                System.out.println("other days");
+        }
+    }
+}
